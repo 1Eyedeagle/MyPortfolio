cb058e91d9a46bded2aea09658c7f3a9c922c7b4
1Eyedeagle <ishusharma882003@gmail.com>
2026-01-28 21:00:55 +0530

    update the lowercase error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/layout/Navbar";
-import Hero from "../components/home/hero";
+import Hero from "../components/home/Hero";
 import About from "../components/home/About";
 import Services from "../components/home/Services";
 import Projects from "../components/home/Projects";
